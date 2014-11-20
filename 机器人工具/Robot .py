a617b59a78f16ef04fe3d9c268ed99d2763e43ce
gaoming655@gmail.com <6753673a>
2014-11-20 14:08:18 +0800

    添加机器人文件

---
+++ 机器人工具/Robot .py
@@ -0,0 +1,58 @@
+import web
+import subprocess
+import shutil
+import os
+import json
+urls = (
+    '/raid', 'Raid',
+    '/','Info',
+    '/ipmi','ipmi',
+    '/install','install',
+    '/reboot','reboot'
+)
+global grub
+grub = """default=0
+timeout=5
+title LeTV Linux PXE install
+    root (hd0,0)
+    kernel /boot/vmlinuz ks="%s" ksdevice=link 
+    initrd /boot/initrd.img
+"""
+app = web.application(urls, globals())
+class Raid:        
+    def GET(self):
+        data = web.input()
+        lv = data.get('lv')
+        disk = data.get('disk',)
+        code = os.system("/bin/sh /root/auto_install.sh --raid %s %s" % (disk,lv))
+        return json.dumps({'code':code})
+class Info:
+    def GET(self):
+        os.system('/bin/sh /root/auto_install.sh --info')
+        filemesg = open("/tmp/log",'r').read()
+        return filemesg
+class install():
+    def GET(self):
+        data = web.input()
+        ks = data.get("ks","conf.ks")
+        ksurl = "http://199.0.0.1/%s" % ks
+        grub_file = open("/mnt/boot/grub/grub.conf",'w')
+        grub_file.write(grub % ksurl)
+        return json.dumps({'code':0})
+class reboot():
+    def GET(self):
+        os.system('reboot')
+        return json.dumps({'reboot':0})
+class ipmi():
+    def GET(self):
+        data = web.input()
+        ip = data.get(ip,None)
+        user = data.get(user,None)
+        pw = data.get(pw,None)
+        if not ip and not user and not pw:
+            return json.dump({'code':1})
+        stat = os.system('/bin/sh /root/auto_install.sh %s %s %s' % (ip,user,pw))
+        return json.dumps({'code':stat})
+
+if __name__ == "__main__":
+    app.run()  
---
+++ 机器人工具/auto_install_machine.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#letv functions auto install  machine bash
+#mady by G.M
+#date: 2014-11-11
+RAID (){
+	inc=`/usr/bin/facter manufacturer`
+	echo $inc|grep HP > /dev/null
+	code=$?
+	disk_lv=$1
+	disk_list=$2
+	if [ $code -ne "0" ];then
+		/opt/MegaRAID/MegaCli/MegaCli64 -CfgLdDel -Lall -a0
+		/opt/MegaRAID/MegaCli/MegaCli64 -CfgLdAdd -r${disk_lv} $disk_list WB Direct -a0
+	else:
+		slot=`hpacucli ctrl all show |awk '{print $6}'`
+		hpacucli ctrl slot=${slot} array A delete forced
+		hpacucli ctrl slot=${slot} create type=ld drives=$disk_list raid=$disk_lv stripesize=1024
+	fi 
+	sdx=`fdisk -l|grep 'Disk /dev/sd'|awk '{print $2}'|tr -d :`
+	dd  if=/dev/zero of=$sdx  count=1
+	sfdisk $sdx -uM < /root/disk.data
+	mkfs.ext4 ${sdx}1
+	mount ${sdx}1  /mnt/
+	grub-install --root-directory=/mnt/ $sdx 
+	cp /root/vmlinuz  /mnt/boot/
+	cp /root/initrd.img  /mnt/boot/
+
+}
+
+INFO(){
+	ip=`facter ipaddress`
+	mac=`facter macaddress`
+	mem=`facter memorysize`
+	cpu=`facter processor0`
+	cat > /tmp/log <<EOF
+	ip:   $ip
+	mac:  $mac
+	mem:  $mem
+	cpu:  $cpu
+EOF
+	/opt/MegaRAID/MegaCli/MegaCli64 -Pdlist -aall|grep Raw >>/tmp/log
+	slot=`hpacucli ctrl all show |awk '{print $6}'`
+	hpacucli  ctrl slot=${slot} pd all show
+}
+key=$1
+case $key in
+	--info)
+		shift
+		INFO 
+		;;
+	--raid)
+		shift
+		if [ $# -lt 2 ];then
+			exit "1"
+		fi
+		RAID $1 $2
+		;;
+esac
