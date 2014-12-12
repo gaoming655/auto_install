3d904a3cd7c174aaeab1c6e42f10b72b04dd2d5a
gaoming655@gmail.com <6753673a>
2014-12-12 18:24:05 +0800

    添加memcached连接方法

--- pxe/views.py
+++ pxe/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth import login,logout,authenticate
 from forms import *
+from django.core.cache import cache
 from auto_install.settings import server_ip,content_ip
 from django.contrib.auth.decorators import login_required
 import requests
